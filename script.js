067c867e1c49d59c7a95f6a1a6960be7da3c73a2
Harsh Agrawal <agrawalharsh1210@gmail.com>
2020-01-26 18:38:12 +0530

    Calculator Project Progate

---
+++ script.js
@@ -0,0 +1,83 @@
+const calculatorScreen = document.querySelector(".calculator-screen")
+const updateScreen = (number) => {
+    calculatorScreen.value = number
+}
+let prevInput = '0'
+let calculationOperator = ''
+let currentInput = '0'
+
+const numbers = document.querySelectorAll(".number")
+const inputNumber = (number) => {
+    if(currentInput === '0'){
+        currentInput = number
+    }
+    else{
+        currentInput += number
+    }
+}
+
+numbers.forEach((number) => {
+    number.addEventListener("click", (event) =>{
+        inputNumber(event.target.value)
+        updateScreen(currentInput)
+    })
+})
+
+const operators = document.querySelectorAll(".operator")
+
+const inputOperator = (operator) =>{
+    prevInput = currentInput
+    calculationOperator = operator
+    currentInput = '0'
+}
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", (event) =>{
+        inputOperator(event.target.value)
+    })
+})
+
+const equalSign = document.querySelector(".equal-sign")
+
+equalSign.addEventListener("click", () =>{
+    calculate()
+    updateScreen(currentInput)
+})
+
+const calculate = () =>{
+    let result = 0
+    switch(calculationOperator){
+        case '+':
+            result = parseInt(prevInput) + parseInt(currentInput)
+            break
+        case '-':
+            result = parseInt(prevInput) - parseInt(currentInput)
+            break
+        case '*':
+            result = parseInt(prevInput) * parseInt(currentInput)
+            break
+        case '/':
+            result = parseInt(prevInput) / parseInt(currentInput)
+            break
+        case '%':
+            result = parseInt(currentInput)/100
+            break
+        default:
+            return
+    }
+    currentInput = result.toString()
+    calculationOperator = ''
+}
+
+const clearBtn = document.querySelector(".all-clear")
+
+const clearAll = () => {
+    prevInput = '0'
+    currentInput = '0'
+    calculationOperator = ''
+}
+
+clearBtn.addEventListener("click", () => {
+    clearAll()
+    updateScreen(currentInput)
+})
